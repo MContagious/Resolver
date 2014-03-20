dc3ef72be829210070083d907b9ced83d9ed5b12
Kishore Relangi <kisree@gmail.com>
2014-03-21 02:14:17 +0530

    Project space is having all required thirdparty files

--- app.js
+++ app.js
@@ -9,13 +9,31 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var bootstrap = require('bootstrap3-stylus');
+var stylus = require('stylus');
+
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .use(bootstrap());
+}
+
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+//app.use(express.favicon());
+
+app.use(stylus.middleware({
+    src: __dirname + '/resources',
+    dest: __dirname + '/public',
+    debug: true,
+    force: true,
+    compile : compile
+}));
+
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
